dc02e8f1046ca7bb1f1a53c5e0bd4a23eda4b57e
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-04-21 00:37:34 +0430

    create ShelfController.java to new pattern service

--- src/main/java/com/grf/library/controller/ShelfController.java
+++ src/main/java/com/grf/library/controller/ShelfController.java
@@ -1,59 +1,46 @@
 package com.grf.library.controller;
 
-import com.grf.library.repository.ShelfRepository;
-import com.grf.library.repository.entity.Shelf;
+import com.grf.library.repository.model.ShelfModel;
+import com.grf.library.service.ShelfService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/shelf")
 public class ShelfController {
 
     @Autowired
-    private ShelfRepository shelfRepo;
+    @Qualifier("shelfServiceImpl")
+    private ShelfService service;
 
     @GetMapping("")
-    public List<Shelf> list() {
-        return shelfRepo.findAll();
+    public ResponseEntity list() {
+        return new ResponseEntity(service.findAll(), HttpStatus.OK);
     }
-
-    @GetMapping(path = "", params = {"page", "size"})
-    public Iterable<Shelf> list(@RequestParam("page") int page, @RequestParam("size") int size) {
-        Pageable paging = PageRequest.of(page, size);
-        return shelfRepo.findAll(paging);
-    }
-
 
     @GetMapping("/{id}")
-    public Shelf show(@PathVariable long id) {
-        Optional<Shelf> found = shelfRepo.findById(id);
-        if (found.isPresent()) {
-            return found.get();
-        }
-        return null;
+    public ResponseEntity show(@PathVariable long id) {
+        ShelfModel model = service.getById(id);
+        return new ResponseEntity(model, HttpStatus.OK);
     }
 
     @PostMapping("")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Shelf store(@RequestBody Shelf shelf) {
-        return shelfRepo.save(shelf);
+    public ResponseEntity store(@RequestBody ShelfModel model) {
+        ShelfModel createdModel = service.save(model);
+        return new ResponseEntity(createdModel, HttpStatus.CREATED);
     }
 
     @PatchMapping(path = "/{id}", consumes = "application/json")
-    public Shelf update(@RequestBody Shelf patchShelf, @PathVariable long id) {
-        Optional<Shelf> found = shelfRepo.findById(id);
-        if (found.isPresent()) {
-            Shelf shelf = found.get();
-            shelf.setShelfNO(patchShelf.getShelfNO());
-            shelf.setFloor(patchShelf.getFloor());
-            shelf.setDescription(patchShelf.getDescription());
-            return shelfRepo.save(shelf);
+    public ResponseEntity update(@RequestBody ShelfModel pathModel, @PathVariable long id) {
+        ShelfModel foundModel = service.getById(id);
+        if (foundModel != null) {
+            foundModel.setShelfNO(pathModel.getShelfNO());
+            foundModel.setFloor(pathModel.getFloor());
+            foundModel.setDescription(pathModel.getDescription());
+            return new ResponseEntity(service.save(foundModel), HttpStatus.OK);
         }
         return null;
     }
@@ -61,9 +48,9 @@ public class ShelfController {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable long id) {
-        Optional<Shelf> found = shelfRepo.findById(id);
-        if (found.isPresent() && found.get().getId() == id) {
-            shelfRepo.deleteById(id);
+        ShelfModel foundModel = service.getById(id);
+        if (foundModel.getId() == id) {
+            service.deleteById(id);
         }
     }
 
