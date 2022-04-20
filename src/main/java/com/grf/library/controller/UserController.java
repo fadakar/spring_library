96c01b9d041dd15a6ff3f408eb22f72060a2bec6
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-04-21 00:24:52 +0430

    update UserController.java to new pattern with service

--- src/main/java/com/grf/library/controller/UserController.java
+++ src/main/java/com/grf/library/controller/UserController.java
@@ -1,65 +1,48 @@
 package com.grf.library.controller;
 
-import com.grf.library.repository.UserRepository;
-import com.grf.library.repository.entity.User;
-import org.apache.coyote.Response;
+import com.grf.library.repository.model.UserModel;
+import com.grf.library.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepo;
-
-    @Autowired
+    @Qualifier("userServiceImpl")
+    private UserService service;
 
     @GetMapping("")
     public ResponseEntity list() {
-        return new ResponseEntity(userRepo.findAll(), HttpStatus.OK);
+        return new ResponseEntity(service.findAll(), HttpStatus.OK);
     }
-
-    @GetMapping(path = "", params = {"page", "size"})
-    public Iterable<User> list(@RequestParam("page") int page, @RequestParam("size") int size) {
-        Pageable paging = PageRequest.of(page, size);
-        return userRepo.findAll(paging);
-    }
-
 
     @GetMapping("/{id}")
-    public User show(@PathVariable long id) {
-        Optional<User> found = userRepo.findById(id);
-        if (found.isPresent()) {
-            return found.get();
-        }
-        return null;
+    public ResponseEntity show(@PathVariable long id) {
+        UserModel model = service.getById(id);
+        return new ResponseEntity(model, HttpStatus.OK);
     }
 
     @PostMapping("")
-    @ResponseStatus(HttpStatus.CREATED)
-    public User store(@RequestBody User user) {
-        return userRepo.save(user);
+    public ResponseEntity store(@RequestBody UserModel userModel) {
+        UserModel createdModel = service.save(userModel);
+        return new ResponseEntity(createdModel, HttpStatus.CREATED);
     }
 
     @PatchMapping(path = "/{id}", consumes = "application/json")
-    public User update(@RequestBody User patchUser, @PathVariable long id) {
-        Optional<User> found = userRepo.findById(id);
-        if (found.isPresent()) {
-            User user = found.get();
-            user.setAdmin(patchUser.isAdmin());
-            user.setUsername(patchUser.getUsername());
-            user.setPassword(patchUser.getPassword());
-            user.setEmail(patchUser.getEmail());
-            user.setDescription(patchUser.getDescription());
-            return userRepo.save(user);
+    public ResponseEntity update(@RequestBody UserModel pathModel, @PathVariable long id) {
+        UserModel foundModel = service.getById(id);
+        if (foundModel != null) {
+            foundModel.setAdmin(pathModel.isAdmin());
+            foundModel.setUsername(pathModel.getUsername());
+            foundModel.setPassword(pathModel.getPassword());
+            foundModel.setEmail(pathModel.getEmail());
+            foundModel.setDescription(pathModel.getDescription());
+            return new ResponseEntity(service.save(foundModel), HttpStatus.OK);
         }
         return null;
     }
@@ -67,9 +50,9 @@ public class UserController {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable long id) {
-        Optional<User> found = userRepo.findById(id);
-        if (found.isPresent() && found.get().getId() == id) {
-            userRepo.deleteById(id);
+        UserModel foundModel = service.getById(id);
+        if (foundModel.getId() == id) {
+            service.deleteById(id);
         }
     }
 
